4dfdaeacf1e6949f802862f0983f6c405c4a4845
HarishNJ23 <90905707+HarishNJ23@users.noreply.github.com>
2021-10-06 09:43:18 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+function openchat()
+{
+    
+    
+        document.getElementById("container").style.display="inline";
+}
+   
+function closechat()
+{
+        document.getElementById("container").style.display="none";
+}
